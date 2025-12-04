f30a44f0e55367f0947af003fa31f67a924d44f7
Nikita Golovkin <golovkinnv7@gmail.com>
2025-12-04 18:54:14 +0000

    fix homework

--- src/C_hw5.1_bracketBalance/C_hw5.1_bracketBalance.c
+++ src/C_hw5.1_bracketBalance/C_hw5.1_bracketBalance.c
@@ -9,40 +9,41 @@ int main(void)
     char s[] = "{    [1234576(1111   fwgqgqfd)]qjfbkhbfkh12878e f+4d 92+}";
     int len = strlen(s);
     char buff;
-    Block *top = newStack();
+    Node *top = newStack();
     for (int i = 0; i < len; i++) {
-	switch (s[i]) {
-	    case '{': case '[': case '(':
-	        top = push(top, s[i]);
-	        break;
+        switch (s[i]) {
+            case '{': case '[': case '(':
+                top = push(top, s[i]);
+                break;
 
 
-	    case '}': case ']': case ')':
-	        buff = peek(top);
-		if (buff == 0) {
-		    printf("Стек пуст, а скобки еще есть\n");
-		    printf("Баланс не выполнен\n");
-		    deleteStack(top);
-		    return 0;
-		}
+            case '}': case ']': case ')':
+                buff = peek(top);
+                if (buff == 0) {
+                    printf("Стек пуст, а скобки еще есть\n");
+                    printf("Баланс не выполнен\n");
+                    deleteStack(top);
+                    return 0;
+                }
 
-		if ((buff == '{' && s[i] == '}') || (buff == '[' && s[i] == ']') || (buff == '(' && s[i] == ')')) {
-		    top = pop(top);
-		    break;
-		}
-		else {
-		    printf("\n");
-		    deleteStack(top);
-		    return 0;
-		}
-	}
+                if ((buff == '{' && s[i] == '}') || (buff == '[' && s[i] == ']') || (buff == '(' && s[i] == ')')) {
+                    top = pop(top);
+                    break;
+                }
+                else {
+                    printf("Баланс не выполнен\n");
+                    deleteStack(top);
+                    return 0;
+                }
+        }
     }
     if (peek(top) == 0) {
-	printf("Баланс выполнен\n");
-	deleteStack(top);
-	return 0;
+        printf("Баланс выполнен\n");
+        deleteStack(top);
     }
+
     else
-	printf("Баланс не выполнен\n");
+        printf("Баланс не выполнен\n");
+        deleteStack(top);
     return 0;
 }
