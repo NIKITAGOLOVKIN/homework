0689db985261839b3c1e2f296e0ef27c0015d63d
Nikita Golovkin <golovkinnv7@gmail.com>
2025-12-04 14:26:12 +0000

    fix homework

--- src/Python_hw5_codingStyle/4.py
+++ src/Python_hw5_codingStyle/4.py
@@ -1,17 +1,29 @@
-def exchange_of_coins(summa, coin1, coin2, coin3):
-    if summa == 0:
+def inputing():
+    while True:
+        amount = input("Введите число ")     #не пропускаем пользователя дальше пока он
+        for i in amount:                     #не введет неотрицательное целое число
+            if not (i.isdigit()):
+                break
+        else:
+            return int(amount)
+
+
+
+
+def exchange_of_coins(amount, coin1, coin2, coin3):
+    if amount == 0:
         print("-42!")        #если сумма равна 0 то ее разменять нельзя
         return
 
-    for count_coin3 in range(summa // coin3, -1, -1):
-        for count_coin2 in range(summa // coin2, -1, -1):   #берем сколько возможно монет разным номиналом и перебираем
-            for count_coin1 in range(summa // coin1, -1, -1):
-                if count_coin3*coin3 + count_coin2*coin2 + count_coin1*coin1 == summa:  #если получили сумму,
-                    print("Можно разменять сумму", summa, end=" ")                      #печатаем размен
+    for count_coin3 in range(amount // coin3, -1, -1):
+        for count_coin2 in range(amount // coin2, -1, -1):   #берем сколько возможно монет разным номиналом и перебираем
+            for count_coin1 in range(amount // coin1, -1, -1):
+                if count_coin3*coin3 + count_coin2*coin2 + count_coin1*coin1 == amount:
+                    print("Можно разменять сумму", amount, end=" ")
                     if count_coin3 != 0:
                         print(count_coin3, "монетами номиналом", coin3, end=" ")
                     if count_coin2 != 0:
-                        print(count_coin2, "монетами номиналом", coin2, end=" ")        #печать размена
+                        print(count_coin2, "монетами номиналом", coin2, end=" ")
                     if count_coin1 != 0:
                         print(count_coin1, "монетами номиналом", coin1)
                     break
@@ -32,21 +44,10 @@ def exchange_of_coins(summa, coin1, coin2, coin3):
 
 
 
-name = "nikita"
-surname = "golovkin"                                   #определяем монеты
-ot = "viktorovic"
-coin1, coin2, coin3 = len(name), len(surname), len(ot)
+NAME = "nikita"
+SURNAME = "golovkin"                                   #определяем монеты
+PATRONYMIC = "viktorovic"
+coin1, coin2, coin3 = len(NAME), len(SURNAME), len(PATRONYMIC)
 
-
-s = "1234567890"
-while True:
-    summa = input("Введите число ")     #не пропускаем пользователя дальше пока он не введет неотрицательное целое число
-    for i in summa:
-        if i not in s:
-            break
-    else:
-        summa = int(summa)
-        break
-
-
-exchange_of_coins(summa, coin1, coin2, coin3)
+amount = inputing()
+exchange_of_coins(amount, coin1, coin2, coin3)
