f8b6752185277c18ead735adebd2eaeff8aa3c5e
Nikita Golovkin <golovkinnv7@gmail.com>
2025-12-24 15:00:55 +0000

    fix homework

--- src/C_hw5.2_sortingStation/C_hw5.2_sortingStation.c
+++ src/C_hw5.2_sortingStation/C_hw5.2_sortingStation.c
@@ -1,118 +1,81 @@
-#include <stdio.h>
-#include <string.h>
 #include "stack.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-
-void priority(char op1, char op2, int *n1, int *n2)   //функция для определения приоритетов операторов, где оп1 это оператор из стека, а оп2 оператор из строки
+int priority(char op)
 {
-    if ((op1 == '*') || (op1 == '/')) {
-	*n1 = 2;
+    if (op == '*' || op == '/')
+        return 2;
+    if (op == '+' || op == '-')
+        return 1;
+    return 0;
+}
+
+int convertToPostfix(char* infix, char* postfix)
+{
+    Node* top = newStack();
+    int tracker = 0;
+    int len = strlen(infix);
+
+    for (int i = 0; i < len; ++i) {
+        switch (infix[i]) {
+        case '+':
+        case '/':
+        case '-':
+        case '*':
+            char current_op = infix[i];
+            while (top != NULL && peek(top) != '(') {
+                char stack_op = peek(top);
+                if (priority(stack_op) < priority(current_op))
+                    break;
+
+                postfix[tracker] = stack_op;
+                tracker++;
+                top = pop(top);
+            }
+            top = push(top, current_op);
+            break;
+
+        case '(':
+            top = push(top, infix[i]);
+            break;
+
+        case ')': // идем по стеку до '(' и все операторы отправляем в массив
+            while (top != NULL && peek(top) != '(') {
+                postfix[tracker] = peek(top);
+                tracker++;
+                top = pop(top);
+            }
+            if (top != NULL && peek(top) == '(') {
+                top = pop(top); // Удаляем '(' из стека
+            }
+            break;
+
+        case ' ': // пробелы просто пропускаем
+            break;
+
+        default: // если элемент цифра, отправляем в массив
+            postfix[tracker] = infix[i];
+            tracker++;
+            break;
+        }
     }
-    else {
-	*n1 = 1;
-    }
-    if ((op2 == '*') || (op2 == '/')) {
-	*n2 = 2;
-    }
-    else {
-	*n2 = 1;
+
+    while (top != NULL) { // если в стеке еще остались операторы, отправляем их в массив
+        postfix[tracker] = peek(top);
+        ++tracker;
+        top = pop(top);
     }
 }
 
-
-
 int main(void)
 {
-    char infix[] = "(1 + 1) * 2 + (3 + 3) * 4";
+    char* infix = "(1 + 2) * 3";
     printf("Инфиксная запись: %s\n\n", infix);
-    int len = strlen(infix);
-    int size = 0;
-    for (int i = 0; i < len; ++i) {                                //считаем сколько в строке операторов и цифр
-	if (infix[i] != '(' && infix[i] != ')' && infix[i] != ' ')
-	    ++size;
-    }
-    char postfix[size];                                          //создаем массив размером сколько посчитали в цикле
-    Block *top = newStack();
-    int tracker = 0;           //индекс свободной ячейки выходного массива
-    char buff;
-
-    for (int i = 0; i < len; ++i) {                //пробегаем по строке
-	switch (infix[i]) {
-	    case '+': case '/': case '-': case '*':
-		if (top == NULL) {                     //если стек пуст просто кидаем туда оператор
-		    top = push(top, infix[i]);
-		    break;
-		}
-
-		buff = peek(top);
-		if (buff == '(') {                     //если в стеке скобка тоже просто кидаем оператор
-		    top = push(top, infix[i]);
-		    break;
-		}
-
-		int n1 = 0;
-		int n2 = 0;
-		priority(buff, infix[i], &n1, &n2);    //пока у оператора из стека приоритет >= приоритета оператора из строки
-		while (n1 >= n2) {                     //операторы из стека отправляем в массив
-		    postfix[tracker] = buff;
-		    ++tracker;
-		    top = pop(top);
-		    if (top == NULL) {
-			break;
-		    }
-		    buff = peek(top);
-		}
-		top = push(top, infix[i]);             //оператор из строки кидаем в стек
-		break;
-
-
-
-	    case '(':
-		top = push(top, infix[i]);
-		break;
-
-
-
-	    case ')':              //идем по стеку до '(' и все операторы отправляем в массив
-		buff = peek(top);
-		while (buff != '(') {
-		    postfix[tracker] = buff;
-		    ++tracker;
-		    top = pop(top);
-		    buff = peek(top);
-		}
-		top = pop(top);
-		break;
-
-
-
-	    case ' ':   //пробелы просто пропускаем
-		break;
-
-
-
-	    default:           //если элемент цифра, отправляем в массив
-		postfix[tracker] = infix[i];
-		++tracker;
-		break;
-	}
-    }
-
-
-    while (top != NULL) {            //если в стеке еще остались операторы, отправляем их в массив
-	postfix[tracker] = peek(top);
-	++tracker;
-	top = pop(top);
-    }
-
-    printf("Постфиксная запись: ");
-    for (int i = 0; i < size; ++i) {
-	printf("%c ", postfix[i]);                //печатаем массив
-    }
-    printf("\n");
-    printf("\n");
-
-
-    deleteStack(top);
+    char* postfix = malloc(strlen(infix) * 2);
+    convertToPostfix(infix, postfix);
+    printf("Постфиксная запись: %s\n", postfix);
+    free(postfix);
     return 0;
 }
